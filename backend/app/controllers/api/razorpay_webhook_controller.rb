06cad9c7a8ae6c237f65402e55a3098261d34fea
vignesh G <vigneshgnanasekaran27@gmail.com>
2025-07-15 19:16:11 +0530

    deploy error fix commented the verify_authenticity_token

--- backend/app/controllers/api/razorpay_webhook_controller.rb
+++ backend/app/controllers/api/razorpay_webhook_controller.rb
@@ -1,5 +1,5 @@
 class Api::RazorpayWebhookController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
 
   def create
     event = request.headers["X-Razorpay-Event"] || params[:event]
